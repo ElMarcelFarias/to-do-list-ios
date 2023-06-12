0e3ee4fdb45545912c85d97151aa003dbd4e37c9
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-06-12 11:03:01 -0300

    Change: Finalizando criação das telas e segues show, próxima etapa são as tableview

---
+++ to-do-list-ios/New Group/Controller/GerenciarTarefasTableController.swift
@@ -0,0 +1,28 @@
+//
+//  GerenciarTarefasTableController.swift
+//  to-do-list-ios
+//
+//  Created by Marcel Leite de Farias on 12/06/23.
+//
+
+import UIKit
+
+class GerenciarTarefasTableController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+
+}
---
+++ to-do-list-ios/New Group/Controller/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginController.swift
+//  to-do-list-ios
+//
+//  Created by Marcel Leite de Farias on 12/06/23.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+
+}
